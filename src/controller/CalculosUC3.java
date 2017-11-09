0542576707865edd0af93b032cf7ccd9ee4c2927
Larissa Sales <larissa.s.sales@outlook.com>
2017-11-09 10:56:32 -0200

    Criada classe CalculosUC3

---
+++ src/controller/CalculosUC3.java
@@ -0,0 +1,31 @@
+package controller;
+
+public class CalculosUC3 implements Calculo {
+	private boolean IsHarmPar;
+	private int NumHarm;
+	private double AmpH1;
+	private double AngH1;
+	private int OrdH1;
+	private double AmpH2;
+	private double AngH2;
+	private int OrdH2;
+	private double AmpH3;
+	private double AngH3;
+	private int OrdH3;
+	private double AmpH4;
+	private double AngH4;
+	private int OrdH4;
+	private double AmpH5;
+	private double AngH5;
+	private int OrdH5;
+	private double AmpH6;
+	private double AngH6;
+	private int OrdH6;
+	
+	
+	@Override
+	public void calcular() {
+		// TODO Auto-generated method stub
+		
+	}
+}
