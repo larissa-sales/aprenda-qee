df9f0f5b706f7bca57fb2eaa3b8ef0b3f3485514
Larissa Sales <larissa.s.sales@outlook.com>
2017-11-12 02:51:34 -0200

    Implementado funções cálculo ondas harmônicas

--- src/controller/CalculosUC3.java
+++ src/controller/CalculosUC3.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CalculosUC3 implements Calculo {
@@ -8,27 +9,29 @@ public class CalculosUC3 implements Calculo {
 
 	private double ampV;
 	private double angV;
-	private boolean IsHarmPar;
-	private int NumHarm;
-	private double AmpH1;
-	private double AngH1;
-	private int OrdH1;
-	private double AmpH2;
-	private double AngH2;
-	private int OrdH2;
-	private double AmpH3;
-	private double AngH3;
-	private int OrdH3;
-	private double AmpH4;
-	private double AngH4;
-	private int OrdH4;
-	private double AmpH5;
-	private double AngH5;
-	private int OrdH5;
-	private double AmpH6;
-	private double AngH6;
-	private int OrdH6;
+	private double fourier;
+	private boolean isHarmPar;
+	private int numHarm;
+	private double ampH1;
+	private double angH1;
+	private int ordH1;
+	private double ampH2;
+	private double angH2;
+	private int ordH2;
+	private double ampH3;
+	private double angH3;
+	private int ordH3;
+	private double ampH4;
+	private double angH4;
+	private int ordH4;
+	private double ampH5;
+	private double angH5;
+	private int ordH5;
+	private double ampH6;
+	private double angH6;
+	private int ordH6;
 	private List<Double> formaOndaFund;
+	private List<Double> formaOndaDistRes;
 	private List<Double> formaOndaH1;
 	private List<Double> formaOndaH2;
 	private List<Double> formaOndaH3;
@@ -36,13 +39,30 @@ public class CalculosUC3 implements Calculo {
 	private List<Double> formaOndaH5;
 	private List<Double> formaOndaH6;
 	
+	public CalculosUC3() {
+		formaOndaFund = new ArrayList<>();
+		formaOndaDistRes = new ArrayList<>();
+		formaOndaH1 = new ArrayList<>();
+		formaOndaH2 = new ArrayList<>();
+		formaOndaH3 = new ArrayList<>();
+		formaOndaH4 = new ArrayList<>();
+		formaOndaH5 = new ArrayList<>();
+		formaOndaH6 = new ArrayList<>();
+	}
+	
 	
 	@Override
 	public void calcular() {
-		// TODO Auto-generated method stub
-		
+		calculaFormaOndaFund();
+		calculaFormaOndaDistRes();
+		calculaFourier();
+		calculaFormaOndaH1();
+		calculaFormaOndaH2();
+		calculaFormaOndaH3();
+		calculaFormaOndaH4();
+		calculaFormaOndaH5();
+		calculaFormaOndaH6();
 	}
-
 
 	//entradas
 	
@@ -64,216 +84,230 @@ public class CalculosUC3 implements Calculo {
 	public void setAngV(double angV) {
 		this.angV = angV;
 	}
-
+	
 	
 	public boolean isIsHarmPar() {
-		return IsHarmPar;
+		return isHarmPar;
 	}
 
 
 	public void setIsHarmPar(boolean isHarmPar) {
-		IsHarmPar = isHarmPar;
+		this.isHarmPar = isHarmPar;
 	}
 
 
 	public int getNumHarm() {
-		return NumHarm;
+		return numHarm;
 	}
 
 
 	public void setNumHarm(int numHarm) {
-		NumHarm = numHarm;
+		this.numHarm = numHarm;
 	}
 
 
 	public double getAmpH1() {
-		return AmpH1;
+		return ampH1;
 	}
 
 
 	public void setAmpH1(double ampH1) {
-		AmpH1 = ampH1;
+		this.ampH1 = ampH1;
 	}
 
 
 	public double getAngH1() {
-		return AngH1;
+		return angH1;
 	}
 
 
 	public void setAngH1(double angH1) {
-		AngH1 = angH1;
+		this.angH1 = angH1;
 	}
 
 
 	public int getOrdH1() {
-		return OrdH1;
+		return ordH1;
 	}
 
 
 	public void setOrdH1(int ordH1) {
-		OrdH1 = ordH1;
+		this.ordH1 = ordH1;
 	}
 
 
 	public double getAmpH2() {
-		return AmpH2;
+		return ampH2;
 	}
 
 
 	public void setAmpH2(double ampH2) {
-		AmpH2 = ampH2;
+		this.ampH2 = ampH2;
 	}
 
 
 	public double getAngH2() {
-		return AngH2;
+		return angH2;
 	}
 
 
 	public void setAngH2(double angH2) {
-		AngH2 = angH2;
+		this.angH2 = angH2;
 	}
 
 
 	public int getOrdH2() {
-		return OrdH2;
+		return ordH2;
 	}
 
 
 	public void setOrdH2(int ordH2) {
-		OrdH2 = ordH2;
+		this.ordH2 = ordH2;
 	}
 
 
 	public double getAmpH3() {
-		return AmpH3;
+		return ampH3;
 	}
 
 
 	public void setAmpH3(double ampH3) {
-		AmpH3 = ampH3;
+		this.ampH3 = ampH3;
 	}
 
 
 	public double getAngH3() {
-		return AngH3;
+		return angH3;
 	}
 
 
 	public void setAngH3(double angH3) {
-		AngH3 = angH3;
+		this.angH3 = angH3;
 	}
 
 
 	public int getOrdH3() {
-		return OrdH3;
+		return ordH3;
 	}
 
 
 	public void setOrdH3(int ordH3) {
-		OrdH3 = ordH3;
+		this.ordH3 = ordH3;
 	}
 
 
 	public double getAmpH4() {
-		return AmpH4;
+		return ampH4;
 	}
 
 
 	public void setAmpH4(double ampH4) {
-		AmpH4 = ampH4;
+		this.ampH4 = ampH4;
 	}
 
 
 	public double getAngH4() {
-		return AngH4;
+		return angH4;
 	}
 
 
 	public void setAngH4(double angH4) {
-		AngH4 = angH4;
+		this.angH4 = angH4;
 	}
 
 
 	public int getOrdH4() {
-		return OrdH4;
+		return ordH4;
 	}
 
 
 	public void setOrdH4(int ordH4) {
-		OrdH4 = ordH4;
+		this.ordH4 = ordH4;
 	}
 
 
 	public double getAmpH5() {
-		return AmpH5;
+		return ampH5;
 	}
 
 
 	public void setAmpH5(double ampH5) {
-		AmpH5 = ampH5;
+		this.ampH5 = ampH5;
 	}
 
 
 	public double getAngH5() {
-		return AngH5;
+		return angH5;
 	}
 
 
 	public void setAngH5(double angH5) {
-		AngH5 = angH5;
+		this.angH5 = angH5;
 	}
 
 
 	public int getOrdH5() {
-		return OrdH5;
+		return ordH5;
 	}
 
 
 	public void setOrdH5(int ordH5) {
-		OrdH5 = ordH5;
+		this.ordH5 = ordH5;
 	}
 
 
 	public double getAmpH6() {
-		return AmpH6;
+		return ampH6;
 	}
 
 
 	public void setAmpH6(double ampH6) {
-		AmpH6 = ampH6;
+		this.ampH6 = ampH6;
 	}
 
 
 	public double getAngH6() {
-		return AngH6;
+		return angH6;
 	}
 
 
 	public void setAngH6(double angH6) {
-		AngH6 = angH6;
+		this.angH6 = angH6;
 	}
 
 
 	public int getOrdH6() {
-		return OrdH6;
+		return ordH6;
 	}
 
 
 	public void setOrdH6(int ordH6) {
-		OrdH6 = ordH6;
+		this.ordH6 = ordH6;
 	}
 
 	
 	//saídas
 	
+	public double getFourier() {
+		return fourier;
+	}
+	
+	
 	//cálculos saídas
+	
+	private void calculaFourier() {
+		fourier = 0.0;
+	}
+	
 	
 	//gráficos
 	
 	public List<Double> getFormaOndaFund() {
 		return formaOndaFund;
+	}
+	
+	public List<Double> getFormaOndaDistRes(){
+		return formaOndaDistRes;
 	}
 
 
@@ -318,34 +352,76 @@ public class CalculosUC3 implements Calculo {
 			x += 0.0001;
 		}	
 	}
+	
+	
+	private void calculaFormaOndaDistRes() {
+		formaOndaDistRes.clear();
+				
+	}
 
-
+	
 	private void calculaFormaOndaH1() {
 		formaOndaH1.clear();
+		double x = 0;
+		
+		while (x <= 0.1) {
+			formaOndaH1.add(ampH1 * Math.cos(w * x + Math.toRadians(angH1)));
+			x += 0.0001;
+		}
 	}
 
 
 	private void calculaFormaOndaH2() {
 		formaOndaH2.clear();
+		double x = 0;
+		
+		while (x <= 0.1) {
+			formaOndaH2.add(ampH2 * Math.cos(w * x + Math.toRadians(angH2)));
+			x += 0.0001;
+		}
 	}
 
 
 	private void calculaFormaOndaH3() {
 		formaOndaH3.clear();
+		double x = 0;
+		
+		while (x <= 0.1) {
+			formaOndaH3.add(ampH3 * Math.cos(w * x + Math.toRadians(angH3)));
+			x += 0.0001;
+		}
 	}
 
 
 	private void calculaFormaOndaH4() {
 		formaOndaH4.clear();
+		double x = 0;
+		
+		while (x <= 0.1) {
+			formaOndaH4.add(ampH4 * Math.cos(w * x + Math.toRadians(angH4)));
+			x += 0.0001;
+		}
 	}
 
 
 	private void calculaFormaOndaH5() {
 		formaOndaH5.clear();
+		double x = 0;
+		
+		while (x <= 0.1) {
+			formaOndaH5.add(ampH5 * Math.cos(w * x + Math.toRadians(angH5)));
+			x += 0.0001;
+		}
 	}
 
 
 	private void calculaFormaOndaH6() {
 		formaOndaH6.clear();
+		double x = 0;
+		
+		while (x <= 0.1) {
+			formaOndaH6.add(ampH6 * Math.cos(w * x + Math.toRadians(angH6)));
+			x += 0.0001;
+		}
 	}
 }
